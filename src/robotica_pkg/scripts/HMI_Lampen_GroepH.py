1c0d2685dbacb05c5f14fc3da4c53817defd71e0
Teun van den Hanenberg <160735286+Teunhaan@users.noreply.github.com>
2024-06-10 15:03:17 +0200

    Update HMI_Lampen_GroepH.py
    Wijziging van statussen

--- src/robotica_pkg/scripts/HMI_Lampen_GroepH.py
+++ src/robotica_pkg/scripts/HMI_Lampen_GroepH.py
@@ -9,22 +9,30 @@ def callback(data):
 
     # Poort D01 op de arduino werkt niet naar behoren dus er wordt begonnen op poort D02
 
-    if value == "Geel":
-        # Gelezen waarde is Geel upload 000100
-        rospy.loginfo("Geel Single signaal ontvangen")
+    if value == "WachtOpStart":
+        # Gelezen waarde is WachtOpStart upload 100000
+        rospy.loginfo("WachtOpStart signaal ontvangen")
+        pub.publish(32)
+    elif value == "InBedrijf":
+        # Gelezen waarde is Rood upload 000100
+        rospy.loginfo("InBedrijf signaal ontvangen")
         pub.publish(4)
-    elif value == "Rood":
-        # Gelezen waarde is Rood upload 001000
-        rospy.loginfo("Rood signaal ontvangen")
+    elif value == "Storing":
+        # Gelezen waarde is Storing upload 100100
+        rospy.loginfo("Storing signaal ontvangen")
+        pub.publish(16)
+    elif value == "Fout":
+        # Gelezen waarde is Fout upload 001000
+        rospy.loginfo("Fout signaal ontvangen")
         pub.publish(8)
     elif value == "Piep":
         # Gelezen waarde is Piep upload 010000
         rospy.loginfo("Piep signaal ontvangen")
         pub.publish(16)
-    elif value == "Groen":
-        # Gelezen waarde is Groen upload 100000 
-        rospy.loginfo("Groen signaal ontvangen")
-        pub.publish(32)
+    elif value == "Noodstop":
+        # Gelezen waarde is Noodstop upload 011000 
+        rospy.loginfo("Noodstop signaal ontvangen")
+        pub.publish(24)
     else:
         rospy.logwarn("Received unexpected value: %s", value)
 

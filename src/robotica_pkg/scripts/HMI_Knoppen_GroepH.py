71da0029c83b36546b8eaeafe6ed411c9af6b316
ASchure <am.schure@student.avans.nl>
2024-06-18 12:02:12 +0200

    Update HMI_Knoppen_GroepH.py

--- src/robotica_pkg/scripts/HMI_Knoppen_GroepH.py
+++ src/robotica_pkg/scripts/HMI_Knoppen_GroepH.py
@@ -3,39 +3,54 @@
 import rospy
 from std_msgs.msg import UInt8, String
 
-def callback(data):
-    pub = rospy.Publisher('/Signaal', String,  queue_size=1)
-    value = data.data
-    if value > 0:
-        if value == 1:
-            # Gelezen waarde is 1 waarde Start wordt geupload naar topic /Signaal 
-            rospy.loginfo("Waarde 1: Start")
-            pub.publish('Start')
-        elif value == 2:
-            # Gelezen waarde is 2 waarde Start Single wordt geupload naar topic /Signaal 
-            rospy.loginfo("Waarde 2: Start Cyclus")
-            pub.publish('StartCyclus')
-        elif value == 4:
-            # Gelezen waarde is 4 waarde Stop wordt geupload naar topic /Signaal 
-            rospy.loginfo("Waarde 4: Stop")
-            pub.publish('Stop')
-        else:
-            rospy.logwarn("Onjuiste waarde ontvangen: %d", value)
-    else:
-        pass
+class SignalPublisher:
+    def __init__(self):
+        self.pub = rospy.Publisher('/Signaal', String, queue_size=1)
+        self.last_value = None
+        self.stop_active = False
 
-def listener():
-    # Start de ROS node
-    rospy.init_node('Knoppen_listener')
-    
-    # Subscriber op '/avans/buttons/state' met inhoud integer
-    rospy.Subscriber('/avans/buttons/state', UInt8, callback)
-    
-    # Keep the node running
-    rospy.spin()
+    def callback(self, data):
+        value = data.data
+        if value > 0 and value != self.last_value:
+            if value == 1:
+                # Gelezen waarde is 1 waarde Start wordt geupload naar topic /Signaal 
+                rospy.loginfo("Waarde 1: Start")
+                self.pub.publish('Start')
+                self.stop_active = False  # Stop het "Stop" signaal als "Start" wordt ontvangen
+            elif value == 2:
+                # Gelezen waarde is 2 waarde Start Single wordt geupload naar topic /Signaal 
+                rospy.loginfo("Waarde 2: Start Cyclus")
+                self.pub.publish('StartCyclus')
+                self.stop_active = False  # Stop het "Stop" signaal als "StartCyclus" wordt ontvangen
+            elif value == 4:
+                # Gelezen waarde is 4 waarde Stop wordt geupload naar topic /Signaal 
+                rospy.loginfo("Waarde 4: Stop")
+                self.pub.publish('Stop')
+                self.stop_active = False  # Houd het "Stop" signaal actief
+            else:
+                rospy.logwarn("Onjuiste waarde ontvangen: %d", value)
+            self.last_value = value
+        elif value == 0:
+            self.last_value = None
+
+    def listener(self):
+        # Start de ROS node
+        rospy.init_node('Knoppen_listener')
+        
+        # Subscriber op '/avans/buttons/state' met inhoud integer
+        rospy.Subscriber('/avans/buttons/state', UInt8, self.callback)
+        
+        # Loop om het "Stop" signaal actief te houden als er geen "Start" of "StartCyclus" wordt ontvangen
+        rate = rospy.Rate(10)  # 10 Hz
+        while not rospy.is_shutdown():
+            if self.stop_active:
+                rospy.loginfo("Stop signaal blijft actief...")
+                self.pub.publish('Stop')
+            rate.sleep()
 
 if __name__ == '__main__':
     try:
-        listener()
+        signal_publisher = SignalPublisher()
+        signal_publisher.listener()
     except rospy.ROSInterruptException:
         pass
